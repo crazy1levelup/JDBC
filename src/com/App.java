e10a688e149bf86a864f6429faa532aac250010f
crazy1levelup <trif.eduard@yahoo.ro>
2018-10-23 10:26:54 +0300

    Formatting

--- src/com/App.java
+++ src/com/App.java
@@ -10,24 +10,21 @@ import java.util.List;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CainiDaoImpl cdi = new CainiDaoImpl();
         //Caini caini = new Caini(3,"Mucus", "Doberman",5);
-       // cdi.insert(caini);
+        // cdi.insert(caini);
 
-       // Caini caini = cdi.selectById(3);
+        // Caini caini = cdi.selectById(3);
         //System.out.println(caini.getId() +" " +  caini.getName()+ " " + caini.getRace() +" " + caini.getAngerScale());
-
 
         //cdi.delete(3);
         //Caini caini2 =  new Caini("Thomas", "Corgi", 3);
-       //cdi.update(caini2, 3);
+        //cdi.update(caini2, 3);
 
         List<Caini> cainiList = cdi.selectAll();
-        for(Caini caine : cainiList ){
-            System.out.println(caine.getId() + " " + caine.getName() + " " + caine.getRace() + " " + caine.getAngerScale() );
+        for (Caini caine : cainiList) {
+            System.out.println(caine.getId() + " " + caine.getName() + " " + caine.getRace() + " " + caine.getAngerScale());
         }
-
-
     }
 }
