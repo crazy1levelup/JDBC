af7fdaf23a312371eecc8e023c8a82fc459fb486
crazy1levelup <trif.eduard@yahoo.ro>
2018-10-23 10:33:18 +0300

    Formatting

--- src/com/dao/CainiDao.java
+++ src/com/dao/CainiDao.java
@@ -7,10 +7,13 @@ import java.util.List;
 public interface CainiDao {
 
     void insert(Caini caine);
+
     Caini selectById(int id);
 
     List<Caini> selectAll();
+
     void delete(int id);
+
     void update(Caini caine, int id);
 }
 
